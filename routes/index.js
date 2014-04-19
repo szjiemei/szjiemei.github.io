393a2ddc8c0cfe8edd3be4595f40001d69f76517
unknown <caiqd@Deborah_NB.padc.com.sg>
2014-04-19 22:45:05 +0800

    minor change of title and header

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 exports.index = function(req, res){
-    res.render('index', { title: 'Wedding of Huang Qi & Cai Qidan' });
+    res.render('index', { header: 'Wedding of Huang Qi & Cai Qidan', 
+    					title:'Site in Construction. Please wait for HQ&QD\'s invitation of wedding.' });
 };
