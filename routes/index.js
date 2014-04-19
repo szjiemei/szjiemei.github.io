a93731eaa9aab675dee83f2777384160f61a9963
unknown <caiqd@Deborah_NB.padc.com.sg>
2014-04-19 20:06:56 +0800

    A fixed navigation bar on top

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res){
-    res.render('index', { title: 'Polls' });
+    res.render('index', { title: 'Wedding of Huang Qi & Cai Qidan' });
 };
--- web.js
+++ web.js
@@ -1,15 +1,31 @@
 // web.js
 var express = require("express");
-var logfmt = require("logfmt");
+var logfmt = require('logfmt');
+var routes = require("./routes");
+var http = require("http");
+var path = require('path');
+
 var app = express();
+var server = http.createServer(app);
+
+app.set('port', process.env.VCAP_APP_PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Huang Qi and Cai Qidan wedding website\n Construction in process!');
+
+// Handle Errors gracefully
+app.use(function(err, req, res, next) {
+	if(!err) return next();
+	console.log(err.stack);
+	res.json({error: true});
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// Main App Page
+app.get('/', routes.index);
+
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
