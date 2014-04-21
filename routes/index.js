f4faaf1e2cdbd57fabb0f53a7bcd77461165f713
Huang Qi <huangqiyx@outlook.com>
2014-04-21 17:38:43 +0800

    mobile control added. Add a simple mobile page display.

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,8 @@ exports.index = function(req, res){
     res.render('index', { header: 'Wedding of Huang Qi & Cai Qidan', 
     					title:'Site in Construction. Please wait for HQ&QD\'s invitation of wedding.' });
 };
+
+exports.mobile = function(req, res){
+    res.render('mobile', { header: 'Wedding of Huang Qi & Cai Qidan', 
+    					title:'Mobile Site in Construction.' });
+};
